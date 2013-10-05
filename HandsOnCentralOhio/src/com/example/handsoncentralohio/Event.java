4200fea0e400da82ac9b334df24e402eced12cac
ben_a_davis <ben.a.davis@hotmail.com>
2013-10-05 02:30:25 -0400

    OMG this has to work.

--- HandsOnCentralOhio/src/com/example/handsoncentralohio/Event.java
+++ HandsOnCentralOhio/src/com/example/handsoncentralohio/Event.java
@@ -3,16 +3,25 @@ package com.example.handsoncentralohio;
 public class Event {
 
 	private int id;
-	private String opportunityName;
-	private String city;
-	private String country;
+	private String name;
 	private String description;
 	
 	public void setID(int i) {
 		this.id = i;
 	}
-	
 	public int getID(){
 		return this.id;
+	}
+	public void setName(String n) {
+		this.name = n;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setDescr(String d) {
+		this.description = d;
+	}
+	public String getDescr() {
+		return this.description;
 	}
 }
