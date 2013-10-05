166fad07f1dd465b0d3be14d187d20acd0fff41f
unknown <gentryb@GENTRYB1.americas.hpqcorp.net>
2013-10-05 00:50:49 -0400

    Added login layout, not tied to rest of project

--- HandsOnCentralOhio/gen/com/example/handsoncentralohio/R.java
+++ HandsOnCentralOhio/gen/com/example/handsoncentralohio/R.java
@@ -44,18 +44,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
+        public static final int action_settings=0x7f090006;
         public static final int button1=0x7f090001;
-        public static final int button2=0x7f090002;
+        public static final int button2=0x7f090005;
         public static final int editText1=0x7f090000;
+        public static final int editText2=0x7f090004;
+        public static final int textView1=0x7f090002;
+        public static final int textView2=0x7f090003;
     }
     public static final class layout {
         public static final int activity_events=0x7f030000;
-        public static final int main_layout=0x7f030001;
+        public static final int activity_login=0x7f030001;
+        public static final int main_layout=0x7f030002;
     }
     public static final class menu {
         public static final int events=0x7f080000;
-        public static final int main_layout=0x7f080001;
+        public static final int login=0x7f080001;
+        public static final int main_layout=0x7f080002;
     }
     public static final class string {
         public static final int account_button=0x7f060005;
@@ -63,9 +68,9 @@ public final class R {
         public static final int app_name=0x7f060000;
         public static final int event_button=0x7f060004;
         public static final int hello_world=0x7f060002;
-        public static final int keywordSearch=0x7f060007;
         public static final int searchButton=0x7f060006;
         public static final int title_activity_events=0x7f060003;
+        public static final int title_activity_login=0x7f060007;
     }
     public static final class style {
         /** 
