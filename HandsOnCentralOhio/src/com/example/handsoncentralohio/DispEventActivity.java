2d6dc47dfc67001d2015d8100c7dfba898fb7e9c
Jordan Ross <ross.j101@gmail.com>
2013-10-05 09:50:22 -0400

    list works and is clickable (somewhat)

--- HandsOnCentralOhio/src/com/example/handsoncentralohio/DispEventActivity.java
+++ HandsOnCentralOhio/src/com/example/handsoncentralohio/DispEventActivity.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
@@ -40,7 +43,7 @@ public class DispEventActivity extends Activity {
 		
 		for (int i = 0; i < numEvents; i++)
 		{
-			if (curElementOnPage == 0)
+			if (i == 0)
 			{
 				eventDate = (TextView) findViewById(R.id.event1date);
 				eventDate.setText(list.get(i).getStartDate().toString());
@@ -49,7 +52,42 @@ public class DispEventActivity extends Activity {
 				eventDesc = (TextView) findViewById(R.id.event1date);
 				eventDesc.setText(list.get(i).getDescr().toString());
 			}
-			
+			else if (i == 1)
+			{
+				eventDate = (TextView) findViewById(R.id.event2date);
+				eventDate.setText(list.get(i).getStartDate().toString());
+				eventName = (TextView) findViewById(R.id.event2name);
+				eventName.setText(list.get(i).getName().toString());
+				eventDesc = (TextView) findViewById(R.id.event2date);
+				eventDesc.setText(list.get(i).getDescr().toString());
+			}
+			else if (i == 2)
+			{
+				eventDate = (TextView) findViewById(R.id.event3date);
+				eventDate.setText(list.get(i).getStartDate().toString());
+				eventName = (TextView) findViewById(R.id.event3name);
+				eventName.setText(list.get(i).getName().toString());
+				eventDesc = (TextView) findViewById(R.id.event3date);
+				eventDesc.setText(list.get(i).getDescr().toString());
+			}
+			else if (i == 3)
+			{
+				eventDate = (TextView) findViewById(R.id.event4date);
+				eventDate.setText(list.get(i).getStartDate().toString());
+				eventName = (TextView) findViewById(R.id.event4name);
+				eventName.setText(list.get(i).getName().toString());
+				eventDesc = (TextView) findViewById(R.id.event4date);
+				eventDesc.setText(list.get(i).getDescr().toString());
+			}
+			else if (i == 4)
+			{
+				eventDate = (TextView) findViewById(R.id.event5date);
+				eventDate.setText(list.get(i).getStartDate().toString());
+				eventName = (TextView) findViewById(R.id.event5name);
+				eventName.setText(list.get(i).getName().toString());
+				eventDesc = (TextView) findViewById(R.id.event5date);
+				eventDesc.setText(list.get(i).getDescr().toString());
+			}
 		}
 	}
 
@@ -84,6 +122,15 @@ public class DispEventActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void expandEvent(View view)
+	{
+		//if (view.getId() == R.id.event2date)
+		//{
+			Intent intent = new Intent(this, SelectedEventActivity.class);
+			startActivity(intent);
+		//}
 	}
 
 }
