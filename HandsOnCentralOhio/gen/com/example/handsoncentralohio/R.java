a400a2c088fce3fd077e76bf9f683ce62681b51f
Jordan Ross <ross.j101@gmail.com>
2013-10-04 22:46:45 -0400

    Lots of files. Hoping clicking events will go to events

--- HandsOnCentralOhio/gen/com/example/handsoncentralohio/R.java
+++ HandsOnCentralOhio/gen/com/example/handsoncentralohio/R.java
@@ -10,6 +10,26 @@ package com.example.handsoncentralohio;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int aqua=0x7f040009;
+        public static final int black=0x7f04000f;
+        public static final int blue=0x7f04000d;
+        public static final int fuchsia=0x7f040002;
+        public static final int gray=0x7f040005;
+        public static final int green=0x7f04000c;
+        public static final int hoco_blue=0x7f040011;
+        public static final int hoco_green=0x7f040010;
+        public static final int lime=0x7f04000a;
+        public static final int maroon=0x7f040008;
+        public static final int navy=0x7f04000e;
+        public static final int olive=0x7f040006;
+        public static final int purple=0x7f040007;
+        public static final int red=0x7f040003;
+        public static final int silver=0x7f040004;
+        public static final int teal=0x7f04000b;
+        public static final int white=0x7f040000;
+        public static final int yellow=0x7f040001;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -17,28 +37,32 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090002;
+        public static final int button1=0x7f090000;
+        public static final int button2=0x7f090001;
     }
     public static final class layout {
         public static final int activity_events=0x7f030000;
         public static final int main_layout=0x7f030001;
     }
     public static final class menu {
-        public static final int events=0x7f070000;
-        public static final int main_layout=0x7f070001;
+        public static final int events=0x7f080000;
+        public static final int main_layout=0x7f080001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_events=0x7f050003;
+        public static final int account_button=0x7f060005;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int event_button=0x7f060004;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_events=0x7f060003;
     }
     public static final class style {
         /** 
@@ -62,10 +86,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- HandsOnCentralOhio/src/com/example/handsoncentralohio/MainLayoutActivity.java
+++ HandsOnCentralOhio/src/com/example/handsoncentralohio/MainLayoutActivity.java
@@ -2,7 +2,9 @@ package com.example.handsoncentralohio;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainLayoutActivity extends Activity {
 
@@ -18,6 +20,13 @@ public class MainLayoutActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_layout, menu);
         return true;
+    }
+    
+    public void showEvents (View view)
+    {
+    	// Do something in response to button being preseed
+    	Intent intent = new Intent(this, EventsActivity.class);
+    	startActivity(intent);
     }
     
 }
