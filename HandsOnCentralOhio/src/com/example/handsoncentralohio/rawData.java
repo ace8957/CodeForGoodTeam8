a5ed0b80815cead268c5c8225eea111a2959b9e6
= <=>
2013-10-05 06:51:32 -0400

    Test data in a JAVA array!!!!!!!!!!!!!!!!!!!

---
+++ HandsOnCentralOhio/src/com/example/handsoncentralohio/rawData.java
@@ -0,0 +1,11 @@
+package com.example.handsoncentralohio;
+
+public class rawData {
+	String[][] data = { 
+			{"1", "10/6/2013", "Feed the Homeless", "We will be feeding the poor food from our soup kitchen" },
+			{"2", "10/10/2013", "Teach People to Read Good", "We will be teaching people to read good, and do other things good too." },
+			{"3", "10/11/2013", "Feed the Homeless", "We will be feeding the poor food from our soup kitchen" },
+			{"4", "11/1/2013", "Feed the Homeless", "We will be feeding the poor food from our soup kitchen" },
+			{"5", "11/29/2013", "Dad Day", "We will be performing Dad-like activities for kids without Dads." },			
+	};
+}
